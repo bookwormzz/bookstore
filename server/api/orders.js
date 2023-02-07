8ae46f72ed5b2faa41c240d34badb7b7839ce261
michun10 <michelleun@gmail.com>
2023-02-06 19:45:48 -0500

    removing console log2

--- server/api/orders.js
+++ server/api/orders.js
@@ -42,7 +42,6 @@ app.post("/cart", async (req, res, next) => {
 
 app.put("/cart", async (req, res, next) => {
   try {
-    console.log("req.body", req.body);
     const user = await User.findByToken(req.headers.authorization);
     res.send(await user.removeFromCart(req.body));
   } catch (ex) {
